4727980874225249e4d4cef03c2b0b157483a498
Amanda Huang <aamandahuang@gmail.com>
2022-01-28 11:42:14 -0500

    updated css, html and js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+    function firstClue() {
+        var text1 = document.getElementById("firstclue");
+        (#vis1).click(firstClue() {
+            (#hide).toggleClass("show");
+        })
+    }
+/*      
+const clue1 = document.getElementById("firstclue");
 
-document.querySelector("body").appendChild(h2);
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      firstClue();
+    }
+if (left > 0) {
+          dodger.style.left = `${left - 1}px`;
+        }
+      }
+
+
+
+
+
+      function toggleText() {
+        var text = document.getElementById("demo");
+        if (text.style.display === "none") {
+          text.style.display = "block";
+        } else {
+          text.style.display = "none";
+        }
+      }
+      */
